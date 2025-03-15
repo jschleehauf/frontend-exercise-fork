1a1dd7916b7e1a1abc90d20108a8ad5e599d9aa8
Jarratt S <schleeja@gmail.com>
2025-03-15 17:33:28 -0400

    Added a question heading to show current question. Created styling to make each question a card.

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <h1>{{  questions[question].text }}</h1>
+  <div class="card">
+    <h1>Question {{question + 1}}</h1>
+    <p>{{  questions[question].text }}</p>
     <button @click="question++">Next</button> 
   </div>
 </template>
@@ -23,3 +24,25 @@ export default {
   }
 };
 </script>
+
+<style>
+  .card {
+    border: 1px solid rgb(59, 192, 101);
+    border-radius: 8px;
+    padding: 16px;
+    margin: auto;
+    margin-top: 20px;
+    box-shadow: 2px 2px 10px rgb(0, 0, 0, 0.1);
+    background-color: #c0dcc7;
+    width: 75vw;
+    
+  }
+  .card p{
+    font-size: 18px;
+  }
+  .card h1 {
+    font-size: 20px;
+    margin-top: 4px;
+
+  }
+</style>
