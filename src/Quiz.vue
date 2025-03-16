54329be73ad81f46cd6f69be049a4a9df9d4f032
Jarratt S <schleeja@gmail.com>
2025-03-16 12:23:42 -0400

    Added the functionality to save user responses for each question. When the user reaches the end of the questions all the questions and responses are displayed. Added the conditional html rendering based on this condition.

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="card">
-    <h1>Question {{questionIndex + 1}}</h1>
-    <p>{{  questions[questionIndex].text }}</p>
-    <div v-for="(option, index) in questions[questionIndex].answers" v-bind:key=index>
-      <label>
-        <input type="radio" :value="option" v-model="selectedAnswer"/>
-        {{ option }}
-      </label>
+  <div>
+    <div class="card" v-if="!quizEnded">
+      <h1>Question {{questionIndex + 1}}</h1>
+      <p>{{  questions[questionIndex].text }}</p>
+      <div v-for="(option, index) in questions[questionIndex].answers" v-bind:key=index>
+        <label>
+          <input type="radio" :value="option" v-model="selectedAnswer"/>
+          {{ option }}
+        </label>
+      </div>
+      <button @click="nextQuestion" :disabled="selectedAnswer === null">Next</button>
     </div>
-    <button @click="nextQuestion" :disabled="selectedAnswer === null">Next</button>
+    <!-- If the quiz has ended display all questions and corresponding answers -->
+    <div class="card" v-if="quizEnded">
+      <h2>Quiz Results</h2>
+      <div>
+        <div v-for="(question, index) in questions" :key="index">
+          <h3>{{ "Question " + (index + 1) }}</h3>
+          <h4>{{ question.text }}</h4>
+          <p>Your answer: {{  getSelectedAnswer(index) }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,18 +35,39 @@ export default {
   },
   data() {
     return {
-      questionIndex : 0,            // Position of in questions list.
+      questionIndex : 0,            // Position in questions list.
       isQuestionAnswered : false,   // State for the current question of quiz if a user has made a selection.
       selectedAnswer: null,         // Holds the value of the selected radio button.
+      userResponses: [],            // Holds user response for each question.
       quizEnded : false,            // True if the quiz is complete.
     };
   },
   methods: {
     nextQuestion() {
-      this.questionIndex++;          // Increment the index to move to the next question.
-      this.selectedAnswer = null;         // Reset the value held so that the next question is not answered.
+      // Ensure the userResponses array is initialized
+      if (!this.userResponses) {
+      this.userResponses = [];      // Initialize the array if it's null
+      }
+      
+      this.userResponses.push(this.selectedAnswer); // User selection is added to userResponses array
+      // this.console.log(this.userResponses);      // Check the response array
+      
+      if (this.questionIndex < this.questions.length - 1) {   // Make sure array is in bounds
+        this.questionIndex++;          // Increment the index to move to the next question.
+        this.selectedAnswer = null;    // Reset the value held so that the next question is not answered.
+      } else {                          // Reached end of questions
+        this.quizEnded = true;
+        this.questionIndex = 0;
+      }
+    },
+    getSelectedAnswer(index) {
+      return this.userResponses[index]; // Return the user response which corresponds to the question
     }
-  }
+  },
+  computed: {
+    console: () => console,
+    window: () => window,
+  },
 };
 </script>
 
